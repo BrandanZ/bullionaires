0ce5bbce46be04d5a3b30f9318f70e6264137b97
brandon <branthebuilder321@gmail.com>
2023-12-11 20:10:06 -0500

    Add initial token amount and capped

--- smart_contracts/contracts/BullToken.sol
+++ smart_contracts/contracts/BullToken.sol
@@ -3,9 +3,13 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-contract BrandanToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("BrandanToken", "BRAN") {
-        _mint(msg.sender, initialSupply);
+contract BrandanToken is ERC20Capped {
+    address payable public owner;
+
+    constructor(uint256 cap) ERC20("Bullionaires", "BULL") ERC20Capped(cap * (10 ** decimals())) {
+        owner = msg.sender;
+        _mint(owner, 10000000000000 * (10 ** decimals()));
     }
 }
