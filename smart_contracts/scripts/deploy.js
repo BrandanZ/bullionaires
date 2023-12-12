071097bee388ac04e8059d7a6626cb85b0bb1e62
brandon <branthebuilder321@gmail.com>
2023-12-12 17:04:42 -0500

    Add deploy scripts for hardhat and sepolia

--- smart_contracts/scripts/deploy.js
+++ smart_contracts/scripts/deploy.js
@@ -1,33 +1,20 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const hre = require("hardhat")
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  try {
+    const BullToken = await hre.ethers.getContractFactory("BullToken")
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+    const bullToken = await BullToken.deploy(10000000000000)
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+    await bullToken.waitForDeployment()
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+    console.log("Deployed Contract Instance: ", bullToken.target)
+  } catch (error) {
+    console.error("An error...", error)
+  }
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
