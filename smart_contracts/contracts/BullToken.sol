b649223bbecff1a69e275589d51342db33f78d73
brandon <branthebuilder321@gmail.com>
2023-12-13 12:46:36 -0500

    Add transfer test

--- smart_contracts/contracts/BullToken.sol
+++ smart_contracts/contracts/BullToken.sol
@@ -25,4 +25,29 @@ contract BullToken is ERC20Capped, ERC20Burnable {
             }
         }
     }
+
+    // exchange rate: 1B BULL per 0.1 ETH
+    uint256 public constant tokensPerEth = 10000000000;
+
+    function buyTokens(address recipient) public payable {
+        require(msg.value > 0, "You need to send some ETH");
+        uint256 tokensToSend = (msg.value * tokensPerEth) / (10 ** decimals());
+
+        //check if the contract has enough tokens to send
+        uint256 contractBalance = balanceOf(address(this));
+
+        require(contractBalance >= tokensToSend, "Not enough tokens in the contract");
+
+        _transferFromContract(recipient, tokensToSend);
+    }
+
+    function _transferFromContract(address recipient, uint256 amount) internal {
+        require(balanceOf(address(this)) >= amount, "ERC20: insufficient contract balance");
+        _transfer(address(this), recipient, amount);
+    }
+
+    // receive function calls buyTokens with the original address caller
+    receive() external payable {
+        buyTokens(msg.sender);
+    }
 }
--- smart_contracts/test/BullToken.js
+++ smart_contracts/test/BullToken.js
@@ -14,7 +14,6 @@ describe("BullToken contract", function () {
     // Get the ContractFactory and Signers here.
     Token = await ethers.getContractFactory("BullToken")
     ;[owner, addr1, addr2] = await hre.ethers.getSigners()
-
     bullToken = await Token.deploy(tokenCap)
   })
 
@@ -91,6 +90,78 @@ describe("BullToken contract", function () {
 
       const addr2Balance = await bullToken.balanceOf(addr2.address)
       expect(addr2Balance).to.equal(50)
+    })
+
+    describe("Buying Tokens", function () {
+      let ethToSend
+      let tokensPerEth
+      let expectedTokens
+
+      beforeEach(async function () {
+        const tokensForContract = hre.ethers.parseUnits(
+          "1000000000000000000000",
+          "wei"
+        )
+        await bullToken
+          .connect(owner)
+          .transfer(bullToken.target, tokensForContract)
+
+        ethToSend = hre.ethers.parseEther("0.1")
+
+        tokensPerEth = BigInt((await bullToken.tokensPerEth()).toString())
+
+        expectedTokens = (ethToSend * tokensPerEth) / BigInt(10 ** 18)
+
+        // Logging for debugging
+        console.log("ETH to Send (in Wei):", ethToSend.toString())
+        console.log("Tokens per ETH:", tokensPerEth.toString())
+        console.log("Expected Tokens:", expectedTokens.toString())
+        console.log("Owner address:", owner.address)
+        console.log("Addr1 address:", addr1.address)
+        console.log("Addr2 address:", addr2.address)
+
+        const contractBalance = await bullToken.balanceOf(bullToken.target)
+        console.log("BullToken contract balance:", contractBalance.toString())
+
+        // Fetch the cap for debugging
+        const cap = await bullToken.cap()
+        console.log("Token Cap:", cap.toString())
+      })
+
+      it("should have enough tokens in the contract for distribution", async function () {
+        console.log(bullToken.target)
+        console.log("Deployed Contract Address:", bullToken.target)
+        const contractBalance = await bullToken.balanceOf(bullToken.target)
+        console.log(
+          "Contract's Initial Token Balance:",
+          contractBalance.toString()
+        )
+        expect(contractBalance).to.be.at.least(expectedTokens)
+      })
+
+      it("should fail when no ETH is sent", async function () {
+        await expect(
+          bullToken.connect(addr1).buyTokens(addr1.address)
+        ).to.be.revertedWith("You need to send some ETH")
+      })
+
+      it("should allow users to buy tokens when they send 0.1 ETH", async function () {
+        // Record addr1's BULL token balance before the purchase
+        const initialBalance = await bullToken.balanceOf(addr1.address)
+        console.log("Initial BULL Token Balance:", initialBalance.toString())
+
+        // Perform the token purchase
+        await bullToken
+          .connect(addr1)
+          .buyTokens(addr1.address, { value: ethToSend })
+
+        // Calculate the expected balance after the purchase
+        const finalBalance = await bullToken.balanceOf(addr1.address)
+        console.log("Final BULL Token Balance:", finalBalance.toString())
+
+        // Check if the final balance matches the expected balance
+        expect(finalBalance).to.be.at.least(expectedTokens)
+      })
     })
   })
 })
